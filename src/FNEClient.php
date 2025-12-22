ffa48e397776ab17ba63829ab4d3ed6ef95b0f48
Armel Meledje <meledjearmel@gmail.com>
2025-12-22 15:05:45 +0000

    feat(composer): Phase 1.2 - Configurer composer.json avec métadonnées et dépendances

---
+++ src/FNEClient.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Neocode\FNE;
+
+/**
+ * FNE Client - Point d'entrée principal du package
+ *
+ * @package Neocode\FNE
+ */
+class FNEClient
+{
+    /**
+     * Create a new FNE Client instance.
+     */
+    public function __construct()
+    {
+        // TODO: Injection de dépendances (HttpClient, Cache, Logger)
+    }
+
+    /**
+     * Get the invoice service.
+     *
+     * @return void
+     */
+    public function invoice()
+    {
+        // TODO: Retourner InvoiceService
+    }
+
+    /**
+     * Get the purchase service.
+     *
+     * @return void
+     */
+    public function purchase()
+    {
+        // TODO: Retourner PurchaseService
+    }
+
+    /**
+     * Get the refund service.
+     *
+     * @return void
+     */
+    public function refund()
+    {
+        // TODO: Retourner RefundService
+    }
+}
+
